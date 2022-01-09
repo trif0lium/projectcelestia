29d44189b7e7527d99c34d9d730fe119566b2ecb
trif0lium <gh@fleeingsunlight.dev>
2022-01-09 15:53:02 +0700

    feat(characters): add PrismaModule to imports

--- libs/characters/src/lib/characters.module.ts
+++ libs/characters/src/lib/characters.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@projectcelestia/graphql'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
   exports: [],
