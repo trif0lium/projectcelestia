22a9efc7fe6a21a1fc558d3580214edbaa366455
trif0lium <gh@fleeingsunlight.dev>
2022-01-09 16:40:01 +0700

    build(tools/tasks-runner): taskRunnerOptions -> tasksRunnerOptions

--- tools/use-nx-cloud-tasks-runner.js
+++ tools/use-nx-cloud-tasks-runner.js
@@ -3,6 +3,6 @@ const { join } = require('path');
 
 const nxJsonPath = join(__dirname, '../nx.json');
 const nxJson = readJsonSync(nxJsonPath);
-nxJson.taskRunnerOptions.default.runner = "@nrwl/nx-cloud"
+nxJson.tasksRunnerOptions.default.runner = "@nrwl/nx-cloud"
 
 writeJsonSync(nxJsonPath, nxJson, { spaces: 2 });
