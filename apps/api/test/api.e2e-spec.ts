bdb63a2010ba39fd3460d3d3fae1060c626f2390
trif0lium <gh@fleeingsunlight.dev>
2022-01-11 14:32:01 +0700

    test(api): add mySQLContainer

--- apps/api/test/api.e2e-spec.ts
+++ apps/api/test/api.e2e-spec.ts
@@ -1,11 +1,15 @@
 import { Test } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import { AppModule } from '../src/app/app.module'
+import { StartedMySqlContainer, MySqlContainer } from 'testcontainers'
 
 describe('@projectcelestia/api', () => {
   let app: INestApplication
+  let mySQLContainer: StartedMySqlContainer
 
   beforeAll(async () => {
+    mySQLContainer = await new MySqlContainer().start()
+
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule]
     }).compile()
