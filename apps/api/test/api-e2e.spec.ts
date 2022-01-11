3f13577ab114cb32a5371f298b711b57f99ee3c7
trif0lium <gh@fleeingsunlight.dev>
2022-01-11 16:38:36 +0700

    test(api): prisma migrate deploy

--- apps/api/test/api-e2e.spec.ts
+++ apps/api/test/api-e2e.spec.ts
@@ -2,6 +2,8 @@ import { Test } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import { AppModule } from '../src/app/app.module'
 import { StartedMySqlContainer, MySqlContainer } from 'testcontainers'
+import { execFile } from 'child_process'
+import path = require('path')
 
 describe('@projectcelestia/api', () => {
   let app: INestApplication
@@ -9,6 +11,20 @@ describe('@projectcelestia/api', () => {
 
   beforeAll(async () => {
     mySQLContainer = await new MySqlContainer().start()
+
+    await new Promise((resolve, reject) => {
+      execFile(path.resolve('../../../../node_modules/prisma/build/index.js'), ['migrate', 'deploy'], {
+        env: {
+          'DATABASE_URL': `mysql://${mySQLContainer.getUsername()}:${mySQLContainer.getUserPassword()}@${mySQLContainer.getHost()}:${mySQLContainer.getPort()}/projectcelestia`
+        }
+      }, (error) => {
+        if (error != null) {
+          reject(error.code ?? 1)
+        } else {
+          resolve(0)
+        }
+      })
+    })
 
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule]
