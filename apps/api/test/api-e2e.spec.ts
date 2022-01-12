137d4447ff9b6c3aa246f4160702e11d26f2e343
trif0lium <gh@fleeingsunlight.dev>
2022-01-12 13:22:16 +0700

    test(api-e2e): import -> import *

--- apps/api/test/api-e2e.spec.ts
+++ apps/api/test/api-e2e.spec.ts
@@ -3,7 +3,7 @@ import { Test } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import { AppModule } from '../src/app/app.module'
 import { StartedMySqlContainer, MySqlContainer } from 'testcontainers'
-import execa from 'execa'
+import * as execa from 'execa'
 
 describe('@projectcelestia/api', () => {
   jest.setTimeout(240_000)
